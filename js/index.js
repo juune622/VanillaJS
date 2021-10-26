9e2250286b97d55c265f70b68cd5c90b398ef6b4
juune622 <juune622@naver.com>
2021-10-26 22:17:22 +0900

    ...

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/* console.log('Im Working')
+
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b); */
+
+//string
+//const what = "juune"
+
+//boolean
+//const wat = true;
+
+
+//Number
+//const wat = 666;
+
+//Float
+//const wat = 55.1
+
+//array
+// Array = [], object={}로 작성
+/* const juneInfo  ={
+	name :"junseop",
+	age :"33",
+	gender :"male",
+	tall : 175,
+}
+console.log(juneInfo.gender)
+
+juneInfo.gender ="female"
+
+console.log(juneInfo.gender) */
+
